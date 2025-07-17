409eaa0462e4d8b61bb6020e1d1d48bd1a8d3a27
unknown <sarawutjongkul@gmail.com>
2025-07-17 12:37:36 +0700

    lab02

---
+++ app/routes/Palm_lab02.tsx
@@ -0,0 +1,15 @@
+function MyProfile(){
+ return (
+    <div>
+        <p ClassName="texet-2x1">Palm Profile</p>
+        <img src="/images/Picture-meme-4.1.jpg" ClassName="w-1/4"/>
+        <p ClassName="texet-lg">
+            Name: Sarawut jogkul <br/>
+            Student ID : 026730491006-5 <br/>
+
+        </p>
+    </div>
+ );
+}
+
+export default MyProfile;
